bf50d25408ce3a5757640f378aca99d4a12d6a69
vinniedaboi <kidosol5224@gmail.com>
2022-08-13 21:06:04 +0800

    Initial Commit

---
+++ ArchiveOfOurOwnAPI/ArchiveOfOurOwnAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchiveOfOurOwnAPIApp.swift
+//  ArchiveOfOurOwnAPI
+//
+//  Created by vincent ng soon zheng  on 13/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ArchiveOfOurOwnAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
